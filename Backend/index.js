1490c085aa62dc5856c2afff55f5447a7a6fd7a8
aditya thakur <ad.2barwal@gmail.com>
2025-07-08 16:51:54 +0530

    auth functions

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const app=express();
 require('dotenv').config();
+const cors=require('cors');
 const {connectDb}=require('./db/connect');
 
 const userRouter=require('./routes/user')
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/user',userRouter);
